26a4beec31036b811ba418f8b3d3981b177e7a5e
rajwindersxxx <rajwindersxxx@gmail.com>
2026-05-15 00:11:08 +0530

    updated config

---
+++ lua/config/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  defaults = {
+    file_ignore_patterns = {},
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+      no_ignore = true,
+    },
+  },
+}
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+            exclude = {
+              "node_modules",
+              ".git",
+              "dist",
+              "build",
+              ".astro",
+            },
+          },
+
+          files = {
+            hidden = true,
+            ignored = true,
+            exclude = {
+              "node_modules",
+              ".git",
+              "dist",
+              "build",
+              ".astro",
+            },
+          },
+        },
+      },
+    },
+  },
+}
