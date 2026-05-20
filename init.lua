ab46e8f5f6623011602fbc30457afddca4df0db2
rajwindersxxx <rajwindersxxx@gmail.com>
2026-05-21 01:02:00 +0530

    add oil plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
+require("oil").setup()
 vim.opt.autochdir = false
+vim.keymap.set("n", "<C-z>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-z>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-z>", "<Nop>", { noremap = true, silent = true })
