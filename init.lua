c94dbe081d6900b5495ba406b0dbb7b65bdd3dfd
rajwindersxxx <rajwindersxxx@gmail.com>
2026-05-25 10:02:48 +0530

    update

--- init.lua
+++ init.lua
@@ -5,3 +5,4 @@ vim.opt.autochdir = false
 vim.keymap.set("n", "<C-z>", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-z>", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("v", "<C-z>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,23 @@
+return {
+  "Exafunction/windsurf.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("codeium").setup({
+      -- This fixes your "module 'cmp' not found" error permanently
+      enable_cmp_source = false,
+
+      -- This turns on your preferred inline ghost text style
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          accept = "<Tab>", -- Press Tab to accept the inline suggestion
+          clear = "<C-x>", -- Press Ctrl + x to dismiss it
+          next = "<M-]>", -- Press Alt + ] to see the next alternative
+          prev = "<M-[>", -- Press Alt + [ to see the previous alternative
+        },
+      },
+    })
+  end,
+}
